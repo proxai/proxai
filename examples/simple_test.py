6c6e303ffd56fcdc58a03cf575a94ef40b2a8bef
proxai <github@proxai.co>
2024-06-10 21:37:59 -0700

    bug: cache times updated if cache hits

---
+++ examples/simple_test.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+import proxai as px
+import random
+import time
+
+
+def main():
+  cache_path = f'{Path.home()}/proxai_cache/'
+  logging_path = f'{Path.home()}/proxai_log/ask_about_model/'
+  os.makedirs(cache_path, exist_ok=True)
+  os.makedirs(logging_path, exist_ok=True)
+  px.connect(
+      experiment_name='cache_test/run_2',
+      cache_path=cache_path,
+      logging_path=logging_path,
+      logging_options=px.LoggingOptions(proxdash_stdout=True))
+  random_int = random.randint(1, 1000000)
+  result = px.generate_text(
+      'This is a test message to check if the cache is working or '
+      f'not. {random_int}')
+  print(f'1: {result}')
+  time.sleep(1)
+
+  result = px.generate_text(
+      'This is a test message to check if the cache is working or '
+      f'not. {random_int}')
+  print(f'2: {result}')
+  time.sleep(1)
+
+  result = px.generate_text(
+      'This is a test message to check if the cache is working or '
+      f'not. {random_int}')
+  print(f'3: {result}')
+
+
+if __name__ == '__main__':
+  main()
