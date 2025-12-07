79f47668cbe6aad7f3fc6968985b46abc78f05f8
proxai <github@proxai.co>
2025-12-06 23:05:23 -0500

    feat: huggingface migration done with structured output

--- examples/structured_output.py
+++ examples/structured_output.py
@@ -5,7 +5,7 @@ import proxai as px
 from pprint import pprint
 from pydantic import BaseModel, Field
 
-PROVIDER_MODEL = ('databricks', 'llama-4-maverick')
+PROVIDER_MODEL = ('huggingface', 'deepseek-v3')
 
 TEST_PROMPT = """
 I need you to calculate the payroll costs for a small team.
