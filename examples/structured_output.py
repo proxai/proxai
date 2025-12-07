cbfc8353066a54ec8780f05c4ca38895f52a983c
proxai <github@proxai.co>
2025-12-06 22:46:34 -0500

    feat: cohere migration done for structural output

--- examples/structured_output.py
+++ examples/structured_output.py
@@ -5,7 +5,7 @@ import proxai as px
 from pprint import pprint
 from pydantic import BaseModel, Field
 
-PROVIDER_MODEL = ('grok', 'grok-3-fast-beta')
+PROVIDER_MODEL = ('cohere', 'command-a')
 
 TEST_PROMPT = """
 I need you to calculate the payroll costs for a small team.
