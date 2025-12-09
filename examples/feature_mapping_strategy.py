a6be1a5f0c40a8fcecbabe0630c69b383b0345c8
proxai <github@proxai.co>
2025-12-08 23:15:03 -0500

    feat: gemini new feature mapping integration

--- examples/feature_mapping_strategy.py
+++ examples/feature_mapping_strategy.py
@@ -59,10 +59,11 @@ def simple_test():
   # provider_model = ('openai', 'gpt-4o-mini')
   # provider_model = ('openai', 'gpt-5.1')
   # provider_model = ('claude', 'haiku-4.5')
-  provider_model = ('claude', 'haiku-3')
+  # provider_model = ('claude', 'haiku-3')
+  provider_model = ('gemini', 'gemini-2.5-flash')
 
   px.connect(
-    feature_mapping_strategy=px_types.FeatureMappingStrategy.STRICT)
+    feature_mapping_strategy=px_types.FeatureMappingStrategy.BEST_EFFORT)
 
   model_configs = px._get_model_configs()
   config = copy.deepcopy(model_configs.model_configs_schema)
@@ -76,10 +77,11 @@ def simple_test():
           'response_format::text',
           'response_format::json',
           'response_format::json_schema',
+          'response_format::pydantic',
       ]
   config.version_config.provider_model_configs[
       provider_model[0]][provider_model[1]].features.best_effort = [
-          'response_format::pydantic',
+
       ]
   model_configs.model_configs_schema = config
 
@@ -92,10 +94,10 @@ def simple_test():
   #     provider_model=provider_model,
   #     system=TEST_FEATURES['system'])
 
-  response = px.generate_text(
-      PROMPT,
-      provider_model=provider_model,
-      response_format=TEST_FEATURES['response_format::pydantic'])
+  # response = px.generate_text(
+  #     PROMPT,
+  #     provider_model=provider_model,
+  #     response_format=TEST_FEATURES['response_format::pydantic'])
 
   print(response)
 
