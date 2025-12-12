a799d40a204596ef42aaf91833e69fd9aca7a898
proxai <github@proxai.co>
2025-12-12 18:46:30 -0500

    bug: claude feature_mapping_strategy test is working

--- examples/feature_mapping_strategy.py
+++ examples/feature_mapping_strategy.py
@@ -45,6 +45,7 @@ class SumOfNumbers(BaseModel):
   C: int
 
 TEST_FEATURES = {
+    'prompt': PROMPT,
     'messages': MESSAGES,
     'system': 'You are a helpful assistant.',
     'max_tokens': 1000,
@@ -60,38 +61,35 @@ TEST_FEATURES = {
 
 
 def get_feature_config(
-    feature: str,
-    response_format_feature: str,
+    features: list[str],
+    config_features: list[str],
     endpoint: str,
     support_type: str,
 ):
-  result_config = {
-    'prompt': px_types.EndpointFeatureInfoType(
+  result_config = {}
+  for feature in config_features:
+    result_config[feature] = px_types.EndpointFeatureInfoType(
         supported=[endpoint],
         best_effort=[],
         not_supported=[],
     )
-  }
 
-  result_config[feature] = px_types.EndpointFeatureInfoType(
-      supported=[endpoint] if support_type == 'supported' else [],
-      best_effort=[endpoint] if support_type == 'best_effort' else [],
-      not_supported=[endpoint] if support_type == 'not_supported' else [],
-  )
+  for feature in features:
+    if feature not in config_features:
+      result_config[feature] = px_types.EndpointFeatureInfoType(
+          supported=[endpoint] if support_type == 'supported' else [],
+          best_effort=[endpoint] if support_type == 'best_effort' else [],
+          not_supported=[endpoint] if support_type == 'not_supported' else [],
+      )
 
-  result_config[response_format_feature] = px_types.EndpointFeatureInfoType(
-      supported=[endpoint],
-      best_effort=[],
-      not_supported=[],
-  )
   return result_config
 
 
 def setup_connection_with_feature_and_endpoint(
     provider_model: px_types.ProviderModelIdentifierType,
     feature_mapping_strategy: px_types.FeatureMappingStrategy,
-    feature: str,
-    response_format_feature: str,
+    features: list[str],
+    config_features: list[str],
     endpoint: str,
     support_type: str,
 ):
@@ -103,8 +101,8 @@ def setup_connection_with_feature_and_endpoint(
   provider_model_config = config.version_config.provider_model_configs[
       provider_model[0]][provider_model[1]]
   feature_config = get_feature_config(
-      feature=feature,
-      response_format_feature=response_format_feature,
+      features=features,
+      config_features=config_features,
       endpoint=endpoint,
       support_type=support_type,
   )
@@ -115,53 +113,55 @@ def setup_connection_with_feature_and_endpoint(
 
 def get_test_feature_call_func(
     provider_model: px_types.ProviderModelIdentifierType,
-    feature: str,
-    response_format_feature: str,
+    features: list[str],
 ):
-  if feature != 'messages':
-    call_func = functools.partial(
-      px.generate_text,
-      prompt=PROMPT,
-      provider_model=provider_model)
-  else:
-    call_func = functools.partial(
-      px.generate_text,
-      provider_model=provider_model)
+  call_func = functools.partial(px.generate_text, provider_model=provider_model)
 
-  if not feature.startswith('response_format::'):
-    call_func = functools.partial(
-      call_func, response_format=TEST_FEATURES[response_format_feature])
-
-  if feature == 'messages':
-    call_func = functools.partial(
-        call_func, messages=TEST_FEATURES['messages'])
-  elif feature == 'system':
-    call_func = functools.partial(
-      call_func, system=TEST_FEATURES['system'])
-  elif feature == 'max_tokens':
-    call_func = functools.partial(
-      call_func, max_tokens=TEST_FEATURES['max_tokens'])
-  elif feature == 'temperature':
-    call_func = functools.partial(
-      call_func, temperature=TEST_FEATURES['temperature'])
-  elif feature == 'stop':
-    call_func = functools.partial(
-      call_func, stop=TEST_FEATURES['stop'])
-  elif feature == 'response_format::text':
-    call_func = functools.partial(
-      call_func, response_format=TEST_FEATURES['response_format::text'])
-  elif feature == 'response_format::json':
-    call_func = functools.partial(
-      call_func, response_format=TEST_FEATURES['response_format::json'])
-  elif feature == 'response_format::json_schema':
-    call_func = functools.partial(
-      call_func, response_format=TEST_FEATURES['response_format::json_schema'])
-  elif feature == 'response_format::pydantic':
-    call_func = functools.partial(
-      call_func, response_format=TEST_FEATURES['response_format::pydantic'])
-  elif feature == 'web_search':
-    call_func = functools.partial(
-      call_func, web_search=TEST_FEATURES['web_search'])
+  for feature in features:
+    if feature == 'prompt':
+      call_func = functools.partial(
+          call_func,
+          prompt=TEST_FEATURES['prompt'])
+    elif feature == 'messages':
+      call_func = functools.partial(
+          call_func,
+          messages=TEST_FEATURES['messages'])
+    elif feature == 'system':
+      call_func = functools.partial(
+          call_func,
+          system=TEST_FEATURES['system'])
+    elif feature == 'max_tokens':
+      call_func = functools.partial(
+          call_func,
+          max_tokens=TEST_FEATURES['max_tokens'])
+    elif feature == 'temperature':
+      call_func = functools.partial(
+          call_func,
+          temperature=TEST_FEATURES['temperature'])
+    elif feature == 'stop':
+      call_func = functools.partial(
+          call_func,
+          stop=TEST_FEATURES['stop'])
+    elif feature == 'response_format::text':
+      call_func = functools.partial(
+          call_func,
+          response_format=TEST_FEATURES['response_format::text'])
+    elif feature == 'response_format::json':
+      call_func = functools.partial(
+          call_func,
+          response_format=TEST_FEATURES['response_format::json'])
+    elif feature == 'response_format::json_schema':
+      call_func = functools.partial(
+          call_func,
+          response_format=TEST_FEATURES['response_format::json_schema'])
+    elif feature == 'response_format::pydantic':
+      call_func = functools.partial(
+          call_func,
+          response_format=TEST_FEATURES['response_format::pydantic'])
+    elif feature == 'web_search':
+      call_func = functools.partial(
+          call_func,
+          web_search=TEST_FEATURES['web_search'])
 
   return call_func
 
@@ -169,8 +169,8 @@ def test_feature_compatibility(
     provider_model: px_types.ProviderModelIdentifierType,
     endpoint: str,
     feature_mapping_strategy: px_types.FeatureMappingStrategy,
-    feature: str,
-    response_format_feature: str,
+    features: str,
+    config_features: str,
     support_type: str,
     verbose: bool = True,
 ):
@@ -179,22 +179,21 @@ def test_feature_compatibility(
     print(f'> Provider model           : {provider_model}')
     print(f'> Endpoint                 : {endpoint}')
     print(f'> Feature mapping strategy : {feature_mapping_strategy}')
-    print(f'> Feature                  : {feature}')
-    print(f'> Response format feature  : {response_format_feature}')
+    print(f'> Features                 : {features}')
+    print(f'> Config features          : {config_features}')
     print(f'> Support type             : {support_type}')
   feature_config = setup_connection_with_feature_and_endpoint(
       provider_model=provider_model,
       feature_mapping_strategy=feature_mapping_strategy,
-      feature=feature,
-      response_format_feature=response_format_feature,
+      features=features,
+      config_features=config_features,
       endpoint=endpoint,
       support_type=support_type,
   )
   try:
     response = get_test_feature_call_func(
         provider_model=provider_model,
-        feature=feature,
-        response_format_feature=response_format_feature
+        features=features,
     )()
     if verbose:
       print(f'SUCCESS: {type(response)}:\n{response}')
@@ -209,118 +208,135 @@ def test_feature_compatibility(
     return False
 
 
-def find_endpoints_response_format(
+def find_endpoint_minimum_required_features(
     provider_model: px_types.ProviderModelIdentifierType,
     endpoint: str,
     verbose: bool = True,
 ):
-  if test_feature_compatibility(
-      provider_model=provider_model,
-      endpoint=endpoint,
-      feature_mapping_strategy=px_types.FeatureMappingStrategy.STRICT,
-      feature='response_format::text',
-      response_format_feature='response_format::text',
-      support_type='supported',
-      verbose=verbose,
-  ):
-    return 'response_format::text'
-  elif test_feature_compatibility(
-      provider_model=provider_model,
-      endpoint=endpoint,
-      feature_mapping_strategy=px_types.FeatureMappingStrategy.STRICT,
-      feature='response_format::json',
-      response_format_feature='response_format::json',
-      support_type='supported',
-      verbose=verbose,
-  ):
-    return 'response_format::json'
-  elif test_feature_compatibility(
-      provider_model=provider_model,
-      endpoint=endpoint,
-      feature_mapping_strategy=px_types.FeatureMappingStrategy.STRICT,
-      feature='response_format::json_schema',
-      response_format_feature='response_format::json_schema',
-      support_type='supported',
-      verbose=verbose,
-  ):
-    return 'response_format::json_schema'
-  elif test_feature_compatibility(
-      provider_model=provider_model,
-      endpoint=endpoint,
-      feature_mapping_strategy=px_types.FeatureMappingStrategy.STRICT,
-      feature='response_format::pydantic',
-      response_format_feature='response_format::pydantic',
-      support_type='supported',
-      verbose=verbose,
-  ):
-    return 'response_format::pydantic'
-  else:
-    raise ValueError(
-        f'No response format feature found for {endpoint} and {provider_model}'
-        '\nThis is probably a bug in the provider implementation of ProxAI.')
-
-def test_feature_compatibility_v2(
-    provider_model: px_types.ProviderModelIdentifierType,
-    endpoints: list[str],
-    features: list[str],
-    verbose: bool = True,
-):
-  result_configs = {}
-  endpoint_response_format_features = {}
-  for endpoint in endpoints:
-    response_format_feature = find_endpoints_response_format(
+  test_required_features = [
+      ['prompt', 'response_format::text'],
+      ['prompt', 'response_format::pydantic'],
+      ['prompt', 'max_tokens', 'response_format::text'],
+      ['prompt', 'max_tokens', 'response_format::pydantic'],
+  ]
+  for required_features in test_required_features:
+    if test_feature_compatibility(
         provider_model=provider_model,
         endpoint=endpoint,
-        verbose=False,
-    )
-    endpoint_response_format_features[endpoint] = response_format_feature
-  print(f'Endpoint response format features:')
-  pprint(endpoint_response_format_features)
-  for feature in features:
-    print(f'--------------------------------- {feature} ---------------------------------')
-    result_configs[feature] = {
+        feature_mapping_strategy=px_types.FeatureMappingStrategy.STRICT,
+        features=required_features,
+        config_features=required_features,
+        support_type='supported',
+        verbose=verbose):
+      return required_features
+
+  raise ValueError(
+      f'No response format feature found for {endpoint} and {provider_model}'
+      '\nThis is probably a bug in the provider implementation of ProxAI.')
+
+
+def get_updated_feature_list(
+  test_feature: str,
+  required_features: list[str],
+):
+  result_features = copy.deepcopy(required_features)
+  if test_feature == 'messages' and 'prompt' in result_features:
+    result_features.remove('prompt')
+
+  if test_feature == 'prompt' and 'messages' in result_features:
+    result_features.remove('messages')
+
+  if test_feature.startswith('response_format::'):
+    if 'response_format::text' in result_features:
+      result_features.remove('response_format::text')
+    if 'response_format::json' in result_features:
+      result_features.remove('response_format::json')
+    if 'response_format::json_schema' in result_features:
+      result_features.remove('response_format::json_schema')
+    if 'response_format::pydantic' in result_features:
+      result_features.remove('response_format::pydantic')
+
+  return result_features + [test_feature]
+
+
+def generate_config_for_model(
+    provider_model: px_types.ProviderModelIdentifierType,
+    endpoints: list[str],
+    test_features: list[str],
+    verbose: bool = True,
+    verbose_test_feature_compatibility: bool = False,
+    verbose_find_endpoint_minimum_required_features: bool = False,
+):
+  result_configs = {}
+  endpoint_minimum_required_features = {}
+  for endpoint in endpoints:
+    endpoint_minimum_required_features[
+        endpoint] = find_endpoint_minimum_required_features(
+            provider_model=provider_model,
+            endpoint=endpoint,
+            verbose=verbose_find_endpoint_minimum_required_features)
+  if verbose:
+    print(f'Endpoint minimum required features:')
+    pprint(endpoint_minimum_required_features)
+  for test_feature in test_features:
+    if verbose:
+      print(f'--------------------------------- {test_feature} ---------------------------------')
+    result_configs[test_feature] = {
         'supported': [],
         'best_effort': [],
         'not_supported': [],
     }
     for endpoint in endpoints:
+      features = get_updated_feature_list(
+        test_feature=test_feature,
+        required_features=endpoint_minimum_required_features[endpoint],
+      )
+      config_features = endpoint_minimum_required_features[endpoint]
       if test_feature_compatibility(
           provider_model=provider_model,
           endpoint=endpoint,
           feature_mapping_strategy=px_types.FeatureMappingStrategy.STRICT,
-          feature=feature,
-          response_format_feature=endpoint_response_format_features[endpoint],
+          features=features,
+          config_features=config_features,
           support_type='supported',
-          verbose=verbose,
+          verbose=verbose_test_feature_compatibility,
       ):
-        result_configs[feature]['supported'].append(endpoint)
+        result_configs[test_feature]['supported'].append(endpoint)
       elif test_feature_compatibility(
           provider_model=provider_model,
           endpoint=endpoint,
           feature_mapping_strategy=px_types.FeatureMappingStrategy.BEST_EFFORT,
-          feature=feature,
-          response_format_feature=endpoint_response_format_features[endpoint],
+          features=features,
+          config_features=config_features,
           support_type='best_effort',
-          verbose=verbose,
+          verbose=verbose_test_feature_compatibility,
       ):
-        result_configs[feature]['best_effort'].append(endpoint)
+        result_configs[test_feature]['best_effort'].append(endpoint)
       else:
-        result_configs[feature]['not_supported'].append(endpoint)
-    pprint({feature: result_configs[feature]})
-    input('Press Enter to continue...')
+        result_configs[test_feature]['not_supported'].append(endpoint)
+    if verbose:
+      pprint({test_feature: result_configs[test_feature]})
+      input('Press Enter to continue...')
   return result_configs
 
 
 def main():
-  provider_model = ('openai', 'gpt-4o-mini')
   endpoints = [
-    'chat.completions.create',
-    'beta.chat.completions.parse',
-    'responses.create',
+      'chat.completions.create',
+      'beta.chat.completions.parse',
+      'responses.create',
   ]
+  provider_model = ('openai', 'gpt-4o-mini')
   # provider_model = ('openai', 'gpt-5.1')
-  # provider_model = ('claude', 'haiku-4.5')
+
+  # endpoints = [
+  #     'messages.create',
+  #     'beta.messages.create',
+  #     'beta.messages.parse',
+  # ]
   # provider_model = ('claude', 'haiku-3')
+  # provider_model = ('claude', 'haiku-4.5')
+
   # provider_model = ('gemini', 'gemini-2.5-flash')
   # provider_model = ('grok', 'grok-3-fast-beta')
   # provider_model = ('mistral', 'mistral-small')
@@ -329,10 +345,10 @@ def main():
   # provider_model = ('databricks', 'meta-llama-3-1-8b-it')
   # provider_model = ('huggingface', 'deepseek-v3')
 
-  test_feature_compatibility_v2(
+  generate_config_for_model(
       provider_model=provider_model,
       endpoints=endpoints,
-      features=[
+      test_features=[
           'prompt',
           'messages',
           'system',
@@ -345,7 +361,9 @@ def main():
           'response_format::pydantic',
           'web_search',
       ],
-      verbose=False,
+      verbose=True,
+      verbose_test_feature_compatibility=False,
+      verbose_find_endpoint_minimum_required_features=False,
   )
 
 
