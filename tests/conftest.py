63d6b3f59f144c54bd3534a53f06c20899760120
proxai-dev <admin@proxai.co>
2026-02-15 16:42:05 -0500

    bug: cut back to make chat test workable

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ def pytest_configure(config):
   """Configure pytest before any imports happen."""
   global _ORIGINAL_API_KEY
   _ORIGINAL_API_KEY = os.environ.pop('PROXDASH_API_KEY', None)
-  pytest.model_configs_instance = model_configs.ModelConfigs()
+  # pytest.model_configs_instance = model_configs.ModelConfigs()
 
 
 def pytest_unconfigure(config):
